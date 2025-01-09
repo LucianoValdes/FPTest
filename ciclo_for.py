823ca33d14cdf4bbf99be6e519bc0cc0d35b3633
LucianoValdes <lucianosepulveda927@gmail.com>
2025-01-08 21:52:08 -0300

    ciclo_for

---
+++ ciclo_for.py
@@ -0,0 +1,78 @@
+
+# for J in range(10):
+#     print("TABLA DEL",J+1)
+#     print()
+#     for i in range(10):
+#         print(J+1,"x",i+1,"=",(i+1)*(J+1))
+
+#CALCULAR EL PROMEDIO DE NOTAS CON FOR
+# total=0
+# for i in range(3):
+#     numeros=int(input("ingrese un numero"))
+    
+#     total=total+numeros
+# print((round,total/(i+1)))
+
+# #SABER SI UN NUMERO ES PAR O IMPAR
+# num=int(input("Ingrese un numero"))
+# for i in range(num+1):
+#     if i%2!=0:
+#         print("el numero",i, "no es par")
+    
+#SABER SI EL NUMERO ES PAR
+
+# num=int(input("Ingrese un numero"))
+# for i in range(num+1):
+#     if i%2==0:
+#         print("el numero",i, " es par")
+    
+#SABER CUANTOS CARACTERES TIEN UNA PALABRA 
+
+# PAL="polonia"
+# print(len(PAL))
+
+# for i in PAL:
+#     print(i)
+
+# total=0
+# PAL=input("ingrese una palabra")
+# for i in range(len(PAL)):
+#     print(i+1)
+#     total=total+(i)
+# print("La suma de la cantidad de letras es",total+i+1)
+
+#VERIFICADOR DE CONTRASEÑA
+# cont=1234
+# for i in range(3):
+#     usu=int(input("ingrese la contraseña"))
+#     if usu == cont:
+#         print("bienvenido al sistema")
+#         break
+#     else:
+#         print("Contraseña incorrecta")
+
+#ENCONTRAR EL NUMERO 
+
+NUM=15
+for i in range(5):
+    print("Ingrese un numero, del 1 hasta el 20, Tiene 5 intentos")
+    usu=int(input())
+    if usu == NUM:
+        print("Perfecto acertaste")
+        break
+    else:
+        if usu >= 16 and usu <=20:
+            print("te pasaste")
+        if usu >=0 and usu <=9:
+            print("el numero que ingresaste es muy bajo")
+        else:
+            if usu >= 10 and usu <= 14:
+                print("el numero que ingresaste esta muy cerca , es mas arriba")
+
+    print("ingresaste un valor fuera de rango")
+            
+        
+
+
+
+
